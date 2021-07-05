e92879307cdb452e75d21d2eed2250d300c31943
Oleksandr Lopatin <nitapol77@gmail.com>
2021-07-05 19:08:16 +0300

    Initial commit

---
+++ app/src/main/java/com/example/helloandroid/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.helloandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+// test
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val byButton = findViewById<Button>(R.id.myButton)
+        val myTextView = findViewById<TextView>(R.id.myTextView)
+
+        byButton.setOnClickListener {
+            myTextView.text = "Hello, my name is Oleksandr Lopatin"
+        }
+
+    }
+}
